4045a5379acfef5d493d9142754399abcd649024
cbfbl <cbfbl651@gmail.com>
2019-12-19 13:36:41 +0200

    added price pie and papaParse

--- tabular_handler.js
+++ tabular_handler.js
@@ -1,37 +1,48 @@
-function check(ev) {
-  var input_files = ev.target.files;
-  var curr_reader = new FileReader();
+function initPage() {
   table = new Tabulator("#michelin_table", {
     pagination: "local",
     paginationSize: 10
   });
-  curr_reader.onload = function(e) {
-    var csv_text = e.target.result;
-    var csv_lines = csv_text.split("\n");
-    var table_headers = csv_lines[0].split(",");
-    var i;
-    var table_data = [];
-    for (header of table_headers) {
-      if (header === "name") {
-        table.addColumn({title: header, field: header, headerFilter: true});
-      } else {
-        table.addColumn({title: header, field: header});
+}
+
+function setTableData(ev) {
+  var input_files = ev.target.files;
+  var curr_reader = new FileReader();
+  var table_data = [];
+  Papa.parse(input_files[0], {
+    header: true,
+    complete: function(results) {
+      var table_headers = Object.keys(results.data[0]);
+      for (header of table_headers) {
+        if (header === "name") {
+          table.addColumn({title: header, field: header, headerFilter: true});
+        } else {
+          table.addColumn({title: header, field: header});
+        }
       }
+      table_data = results.data;
+      table_data.pop();
+      table.setData(table_data).then(loadPricePie(table_data));
     }
-    for (i = 1; i < table_headers.length; i++) {
-      var tmp_dict = {};
-      var curr_data = csv_lines[i].split(",");
-      var k;
-      for (k = 0; k < curr_data.length; k++) {
-        tmp_dict[table_headers[k]] = curr_data[k];
-      }
-      table_data.push(tmp_dict);
-    }
-    table.setData(table_data);
-  };
-  curr_reader.readAsText(input_files[0]);
+  });
 }
 
 function downloadTable() {
   table.download("csv", "table_data.csv");
+}
+
+function loadPricePie(table_data) {
+  var chart = am4core.create("pie_price", "PieChart");
+  var tmp_data = [];
+  var price_fields = {$: 0, $$: 0, $$$: 0, $$$$: 0, $$$$$: 0, "N/A": 0};
+  for (var data of table_data) {
+    price_fields[data.price]++;
+  }
+  for (var key of Object.keys(price_fields)) {
+    tmp_data.push({price: key, cost: price_fields[key]});
+  }
+  chart.data = tmp_data;
+  var pieSeries = chart.series.push(new am4charts.PieSeries());
+  pieSeries.dataFields.value = "cost";
+  pieSeries.dataFields.category = "price";
 }
